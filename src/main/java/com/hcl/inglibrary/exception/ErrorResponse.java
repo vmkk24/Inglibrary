107f48a6ad80bed5e98dbeb097ff642d0ca53a87
ashish-irdeto <mythrir@hcl.com>
2019-10-16 16:33:36 +0530

    qedg

---
+++ src/main/java/com/hcl/inglibrary/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.hcl.inglibrary.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+	private String message;
+	private int statusCode;
+
+}
---
+++ src/main/java/com/hcl/inglibrary/exception/InvalidCredentialsException.java
@@ -0,0 +1,12 @@
+package com.hcl.inglibrary.exception;
+
+public class InvalidCredentialsException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidCredentialsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
